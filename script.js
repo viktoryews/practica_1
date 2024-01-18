6cb3ead57dc712d353b6082dd327818d7ea4d568
Viktorya Evsina <viktoryews1997@mail.ru>
2024-01-19 02:08:07 +0300

    правка

--- script.js
+++ script.js
@@ -42,10 +42,6 @@ console.log(factorial(0));
 /** Задание 4 */
 // Напишите рекурсивную функцию findElementByClass, которая принимает корневой элемент дерева DOM и название класса в качестве аргументов и возвращает 
 // первый найденный элемент с указанным классом в этом дереве.
-// *Пример**
-// const rootElement = document.getElementById('root');
-// const targetElement = findElementByClass(rootElement, 'my-class');
-// console.log(targetElement);
 
 let arrElements = [];
 
